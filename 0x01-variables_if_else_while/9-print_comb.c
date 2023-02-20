ba856cbf1426731e81b95159844ac086b04e7dbf
sasdogged <sasdogged@gmail.com>
2023-02-20 14:14:38 -0800

    ppp

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,40 +1,23 @@
 #include <stdio.h>
 
 /**
- * main - entry point
+ * main - main unction
  *
- * Description: Prints all possible different combinations of two digits.
- * Numbers must be separated by ", " followed by a space
- * The two digits must be different
- * 01 and 10 are considered the same combination of the two digits 0 and 1
- * Print only the smallest combination of two digits
- * Numbers should be printed in ascending order, with two digits
- * You can only use the putchar function (every other function is forbidden)
- * You can only use putchar five times maximum in your code
- * You are not allowed to use any variable of type char
- *
- * Return: Always 0 (success)
+ * Return: Always 0
  */
 int main(void)
 {
-    int i = 0;
+    int i;
 
-    while (i < 100)
+    for (i = 0 ; i < 10 ; i++)
     {
-        if (i / 10 < i % 10)
-        {
-            putchar(i / 10 + '0');
-            putchar(i % 10 + '0');
-
-            if (i < 89)
-            {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-        i++;
+		    putchar(i +'0');
+		    if (i !=9)
+	    {
+		    putchar(',');
+		    putchar(' ');
+	    }
     }
-
     putchar('\n');
     return (0);
 }
