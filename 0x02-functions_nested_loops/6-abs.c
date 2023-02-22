988cb5518439be08dbdafbd54d73a308850616c4
sasdogged <sasdogged@gmail.com>
2023-02-22 15:05:02 -0800

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+
+/**
+ * _abs - checks for absolute value
+ *
+ * @a: parameter to be checked
+ *
+ * Return: always a
+ */
+
+int _abs(int a)
+{
+	if (a < 0)
+		a = -(a);
+	else if (a >= 0)
+		a = a;
+	return (a);
+}
